bd5898f20205d498ece9df73227ed4d898cd1b1e
Sandesh Sidanale <sandeshsidanale@gmail.com>
2024-08-11 12:11:16 +0530

    test2

--- HashMapsAndSet/containsDuplicate.js
+++ HashMapsAndSet/containsDuplicate.js
@@ -4,12 +4,15 @@
  */
 export var containsDuplicate = function(nums) {
     let set = new Set();
+    
     let prevSize = set.size;
+    
     for( let i of nums ){
         set.add(i);
         if(prevSize === set.size)
           return true;
         prevSize = set.size;  
     }
+
     return false;
 };
